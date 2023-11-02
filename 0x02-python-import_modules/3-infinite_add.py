2a665a209516b3391a645f1fcf98b89529bcee85
Ronrudy <ruffroony@yahoo.com>
2023-11-02 10:05:09 -0700

    3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+    total_sum = 0
+
+    if len(argv) > 1:  # Check if there are arguments
+        for i in range(1, len(argv)):
+            # Start from index 1 to skip the script name
+            total_sum += int(argv[i])
+
+        print("{}".format(total_sum))
+    else:
+        print("0")
