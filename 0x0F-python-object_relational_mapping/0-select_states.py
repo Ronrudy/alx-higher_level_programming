f073b652b797417e463a038c7ace7b3702243788
Ronrudy <ruffroony@yahoo.com>
2024-02-18 02:34:35 -0800

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,42 @@
 #!/usr/bin/python3
 """
-This script lists all states from the
-database `hbtn_0e_0_usa`.
+Scripts that lists all states from the database hbtn_0e_0_usa.
 """
 
+
 import MySQLdb
-from sys import argv
+import sys
 
-if __name__ == '__main__':
+
+def select_states(username, password, database):
     """
-    Access to the database and get the states
-    from the database.
+    Selects and prints all states from the specified database.
     """
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd=password, db=database)
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states")
-    rows = cur.fetchall()
+    # Create a cursor object using cursor() method
+    cursor = db.cursor()
 
-    for row in rows:
+    # Execute SQL query
+    cursor.execute("SELECT * FROM states ORDER BY id")
+
+    # Fetch all rows and print them
+    for row in cursor.fetchall():
         print(row)
+
+    # Close the cursor and disconnect from server
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    # Check if correct number of arguments provided
+    if len(sys.argv) == 4:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
+        select_states(username, password, database)
+    else:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
