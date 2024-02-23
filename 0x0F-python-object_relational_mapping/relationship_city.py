4c0e203a86aa5545ee80e17a9343240512c375a7
Ronrudy <ruffroony@yahoo.com>
2024-02-23 03:01:19 -0800

    correction of relationships

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
 """
-Start link class to table in database
+This script defines a City class
+to work with MySQLAlchemy ORM.
 """
+
+from relationship_state import Base, State
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-from .relationship_state import Base  # Updated import
+
 
 class City(Base):
-    """
-    City class linked to the 'cities' table.
+    """City class
+
+    Attributes:
+        __tablename__ (str): The table name of the class
+        id (int): The id of the class
+        name (str): The name of the class
+        state_id (int): The state the city belongs to
+
     """
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-
-from .relationship_state import State  # Moved import to the end
-
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
 """
-Start link class to table in database
+This script defines a State class and
+a Base class to work with MySQLAlchemy ORM.
 """
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-from .relationship_city import Base  # Updated import
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
 
 class State(Base):
-    """
-    State class linked to the 'states' table.
+    """State class
+
+    Attributes:
+        __tablename__ (str): The table name of the class
+        id (int): The State id of the class
+        name (str): The State name of the class
+        cities (:obj:`City`): The Cities belongs to State
+
     """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="all, delete")
-
-from .relationship_city import City  # Moved import to the end
-
